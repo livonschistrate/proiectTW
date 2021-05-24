f62a0b4aa56f6ef51f053e87416c6f20cb973fa5
Liviu <liviu.istrate1@gmail.com>
2021-05-24 19:19:42 +0300

    minor HTML fixes + functional mainbutton

---
+++ scripts/mainbutton.js
@@ -0,0 +1,8 @@
+
+function showMenu(){
+    var menu = document.getElementById('menubar');
+    if(menu.style.display === "block")
+        menu.style.display = "none";
+    else
+        menu.style.display = "block";
+}
