79ec20407f711e1dff3a28231ef3c8001beeab08
Liviu <liviu.istrate1@gmail.com>
2021-06-07 17:36:56 +0300

    minor service page fixes

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,17 +1,14 @@
-var accord = document.getElementsByClassName("accord");
-var i;
 
-for(i = 0; i < accord.length; i++) {
-    //accord[i].addEventListener("click", toggleA());
-}
 
 function toggleA(btn){
     //this.classList.toggleA("active");
     var panel = btn.nextElementSibling;
     if(panel.style.display === "block"){
+       
         panel.style.display = "none";
     }
     else{
         panel.style.display = "block";
+        
     }
 }
