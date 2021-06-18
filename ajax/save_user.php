af2cb18d9299e7ae5f02372acb7e4d7eb90b23bf
Liviu <liviu.istrate1@gmail.com>
2021-06-18 22:50:55 +0300

    added account managing for admin

---
+++ ajax/save_user.php
@@ -0,0 +1,34 @@
+<?php
+// se includ fisierele obligatorii pentru configuratia aplicatiei si accesul la baza de date
+include "../include/config.php";
+include "../include/db_auth.php";
+include "../include/auth_ajax.php";
+
+// se initializeaza array-ul care va contine raspunsul
+$ret = array();
+$ret['code'] = 99;
+$ret['message'] = 'A apărut o eroare, reîncărcați pagina și reluați operația.';
+
+if (!isset($_POST['id_user']) ) { // nu au venit din pagina apelanta valorile necesare pentru salvare
+    $ret['code'] = 99;
+    $ret['message'] = 'Valorile de apel sunt incorecte. Reîncărcați pagina și reluați operația.';
+    echo json_encode($ret);
+    die();
+}
+
+$id_user = intval(trim($_POST['id_user']));
+
+if ( $id_user==0) {
+
+} else { // se doreste actualizarea rolului unui user existent
+    $sql = "UPDATE users SET id_role='".$_POST['id_role']."'
+                           WHERE id_user=".$id_user.";";
+    $db->exec($sql);
+    $ret['id_user'] = $id_user; // se intoarce ca raspuns in pagina id-ul cererii
+    $ret['code'] = 1;
+    $ret['message'] = 'Datele comenzii au fost actualizate.';
+}
+
+// se intoarce raspunsul spre pagina care a apelat via AJAX save_request.php
+echo json_encode($ret);
+die();
